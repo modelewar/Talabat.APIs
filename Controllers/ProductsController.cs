1351d465c7d4cfa92877d949e709a83fa33f6088
MO Delewar <delewarmohamed@gmail.com>
2024-12-04 21:17:59 +0200

    Edit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabate.Core.Entites;
+using Talabate.Core.Repositories;
+
+namespace Talabat.APIs.Controllers
+{
+    public class ProductsController : APIBaseController
+    {
+        private readonly IGenaricRepository<Product> _productRepo;
+
+        public ProductsController(IGenaricRepository<Product> productRepo)
+        {
+            _productRepo = productRepo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var Products = await _productRepo.GetAllAsync();
+            
+            return Ok(Products);
+        }
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Product>> GetProduct(int Id)
+        {
+            var Product = await _productRepo.GetByIdAsync(Id);
+
+            return Ok(Product);
+        }
+    }
+}
